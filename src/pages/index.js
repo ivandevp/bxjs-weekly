ccdcccbbca0fba4ca814b5801c80a0d7c3e3391c
ivandevp <medinadiazivan@gmail.com>
2020-02-09 20:04:35 -0600

    Add default gatsby template

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hola banda!</h1>
+    <p>Aquí encontrarán los episodios del Podcast.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
