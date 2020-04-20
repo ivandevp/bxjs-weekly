7f5a31eef1b3d3babc800343c52fa8f4fff66d9e
ivandevp <medinadiazivan@gmail.com>
2020-04-20 14:31:26 -0500

    Actualiza componente de SEO

---
+++ src/components/seo.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+interface Meta {
+  name: string
+  content: string
+}
+
+interface ISEOProps {
+  title: string
+  description?: string
+  lang?: string
+  meta?: Meta[]
+}
+
+const SEO: React.FC<ISEOProps> = ({
+  description,
+  lang = "es",
+  meta = [],
+  title,
+}) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+
+  return (
+    <Helmet>
+      <title>{title}</title>
+    </Helmet>
+  )
+}
+
+export default SEO
