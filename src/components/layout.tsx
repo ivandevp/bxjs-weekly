7bd5fa885ad4b66ed8bb654c6f44b7e9f224b7a8
ivandevp <medinadiazivan@gmail.com>
2020-04-20 14:32:17 -0500

    Actualiza componente de header

---
+++ src/components/layout.tsx
@@ -0,0 +1,60 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { css, jsx } from "@emotion/core"
+
+import Header from "./header"
+import "./layout.css"
+
+interface ILayoutProps {
+  children: React.ReactNode
+}
+
+const Layout: React.FC<ILayoutProps> = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <Header
+        siteTitle={data.site.siteMetadata.title}
+        siteDescription={data.site.siteMetadata.description}
+      />
+      <div
+        css={css`
+          margin: 0 auto;
+          max-width: 960px;
+        `}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <div>
+            Iconos diseñados por{" "}
+            <a
+              href="https://www.flaticon.es/autores/smashicons"
+              title="Smashicons"
+            >
+              Smashicons
+            </a>{" "}
+            from{" "}
+            <a href="https://www.flaticon.es/" title="Flaticon">
+              www.flaticon.es
+            </a>
+          </div>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default Layout
